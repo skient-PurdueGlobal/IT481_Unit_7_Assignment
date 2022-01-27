aa9a786373859d4254efa846794f7ea6256ffbe1
skienterprises-da <douglas_andrzejewski@ski-network.net>
2022-01-27 15:35:36 -0500

    Add project files.

---
+++ IT481_Unit_7_Assignments.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace IT481_Unit_7_Assignment
+{
+    class IT481_Unit_7_Assignments
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ SortingAlgorithms.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IT481_Unit_7_Assignment
+{
+    class SortingAlgorithms
+    {
+        //Variables
+
+        //Constructors
+        public SortingAlgorithms() { }
+
+        //Methods
+
+    }
+}
