432eddbe43cb8549d711ed634e8777b1ed3a18cb
skienterprises-da <douglas_andrzejewski@ski-network.net>
2022-01-29 15:01:24 -0500

    Assignment Complete

--- IT481_Unit_7_Assignments.cs
+++ IT481_Unit_7_Assignments.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 
 namespace IT481_Unit_7_Assignment
 {
@@ -6,6 +8,74 @@ namespace IT481_Unit_7_Assignment
     {
         static void Main(string[] args)
         {
+            #region Define test sets.
+            int[] smallSet = Array_Generator(100);
+            int[] mediumSet = Array_Generator(1000);
+            int[] largeSet = Array_Generator(10000);
+            #endregion
+
+            #region Test first sort algorithm with all data sets.
+            //Small test set.
+            TimeSpan smTestOneSpan = SortingAlgorithms.BubbleSort(smallSet);
+
+            //Medium test set.
+            TimeSpan mdTestOneSpan = SortingAlgorithms.BubbleSort(mediumSet);
+
+            //Large test set.
+            TimeSpan lgTestOneSpan = SortingAlgorithms.BubbleSort(largeSet);
+            #endregion
+
+            #region Test second sort algorithm with all data sets.
+            //Small test set.
+            Stopwatch smWatch = new Stopwatch();
+            smWatch.Start();
+            int[] smSortedArr = SortingAlgorithms.QuickSort(smallSet, 0, smallSet.Length - 1);
+            smWatch.Stop();
+            TimeSpan smTestTwoSpan = smWatch.Elapsed;
+
+            //Medium test set.
+            Stopwatch mdWatch = new Stopwatch();
+            mdWatch.Start();
+            int[] mdSortedArr = SortingAlgorithms.QuickSort(mediumSet, 0, mediumSet.Length - 1);
+            mdWatch.Stop();
+            TimeSpan mdTestTwoSpan = mdWatch.Elapsed;
+
+            //Large test set.
+            Stopwatch lgWatch = new Stopwatch();
+            lgWatch.Start();
+            int[] lgSortedArr = SortingAlgorithms.QuickSort(largeSet, 0, largeSet.Length - 1);
+            lgWatch.Stop();
+            TimeSpan lgTestTwoSpan = lgWatch.Elapsed;
+            #endregion
+
+            //Output of Results.
+            Console.WriteLine();
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("*********************** Unit 7 Assignment Test Results ***********************");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine();
+            Console.WriteLine("                          Test #1 - Bubble Sort    Test #2 - Quick Sort");
+            Console.WriteLine("Small Data Set (100):           " + String.Format("{0:0.0000}", smTestOneSpan.TotalMilliseconds) + "ms                 " + String.Format("{0:0.0000}", smTestTwoSpan.TotalMilliseconds) + "ms");
+            Console.WriteLine("Medium Data Set (2500):         " + String.Format("{0:0.0000}", mdTestOneSpan.TotalMilliseconds) + "ms                 " + String.Format("{0:0.0000}", mdTestTwoSpan.TotalMilliseconds) + "ms");
+            Console.WriteLine("Large Data Set (10000):       " + String.Format("{0:0.0000}", lgTestOneSpan.TotalMilliseconds) + "ms                " + String.Format("{0:0.0000}", lgTestTwoSpan.TotalMilliseconds) + "ms");
+
+            //Open console to view results.
+            Console.Read();
+
+        }
+
+        //Random array generator method.
+        public static int[] Array_Generator(int size)
+        {
+            int[] array = new int[size];
+            Random sNum = new Random();
+
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = sNum.Next(1, size + 1);
+            }
+
+            return array;
         }
     }
 }
--- SortingAlgorithms.cs
+++ SortingAlgorithms.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Text;
 
 namespace IT481_Unit_7_Assignment
@@ -12,6 +14,72 @@ namespace IT481_Unit_7_Assignment
         public SortingAlgorithms() { }
 
         //Methods
+        //Bubble sort from https://medium.com/engineering-hub/https-medium-com-engineering-hub-sorting-algorithms-in-csharp-and-java-4615f6f87696.
+        public static TimeSpan BubbleSort(int[] rawArray)
+        {
+            int[] sortedArr = rawArray.ToArray();
 
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            int n = sortedArr.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < (n - i - 1); j++)
+                {
+                    if (sortedArr[j] > sortedArr[j + 1])
+                    {
+                        //Swap temp and array[i].
+                        int temp = sortedArr[j];
+
+                        sortedArr[j] = sortedArr[j + 1];
+                        sortedArr[j + 1] = temp;
+                    }
+                }
+            }
+
+            watch.Stop();
+
+            return watch.Elapsed;
+        }
+
+        //Merge sort from IT391 Unit 4 Assignment.
+        public static int[] QuickSort(int[] rawArray, int left, int right)
+        {
+            int[] sortedArr = rawArray.ToArray();
+
+            if (sortedArr != null && sortedArr.Length != 0 && left < right)
+            {
+                //Pick the Pivot.
+                int middle = left + (right - left) / 2;
+                int pivot = sortedArr[middle];
+
+                //Make left < pivot and right > pivot.
+                int i = left, j = right;
+                while (i <= j)
+                {
+                    while (sortedArr[i] < pivot)
+                        i++;
+                    while (sortedArr[j] > pivot)
+                        j--;
+                    if (i <= j)
+                    {
+                        int temp = sortedArr[i];
+                        sortedArr[i] = sortedArr[j];
+                        sortedArr[j] = temp;
+                        i++;
+                        j--;
+                    }
+                }
+                //Recursively sort two sub parts.
+                if (left < j)
+                    sortedArr = QuickSort(sortedArr, left, j);
+                if (right > i)
+                    sortedArr = QuickSort(sortedArr, i, right);
+            }
+
+            return sortedArr;
+        }
     }
 }
